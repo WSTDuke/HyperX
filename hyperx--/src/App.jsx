b503a65beb4d5489ed9888d07cfb7ac2727bc468
Nguyen Duc <104844845+DukeCUD@users.noreply.github.com>
2025-11-20 01:49:55 +0700

    19/11: Working on dashboard (estimate:50%)

---
+++ hyperx--/src/App.jsx
@@ -0,0 +1,12 @@
+import DashboardPage from './page/dashboard/DashboardPage'
+
+function App() {
+
+  return (
+    <>
+      <DashboardPage />
+    </>
+  )
+}
+
+export default App
---
+++ hyperx/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <span className="text- text-red-300 font-bold underline">Hello  !</span>
+    </>
+  )
+}
+
+export default App
